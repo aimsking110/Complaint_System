ed4cbcc978392b8fce49f066b5e68fa6f72ebde4
xub911 <47626793+xub911@users.noreply.github.com>
2022-07-17 11:41:55 +0500

    adminPanel GUI update

--- Complaint_System/GUI/adminPanel.Designer.cs
+++ Complaint_System/GUI/adminPanel.Designer.cs
@@ -31,22 +31,23 @@ namespace Complaint_System.GUI
         {
             this.admin_panel = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Delete_Btn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txt_name = new System.Windows.Forms.TextBox();
-            this.txtPassword = new System.Windows.Forms.TextBox();
-            this.txtPhn = new System.Windows.Forms.TextBox();
-            this.txtcnic = new System.Windows.Forms.TextBox();
-            this.Decline_Btn = new System.Windows.Forms.Button();
-            this.Accept_Btn = new System.Windows.Forms.Button();
+            this.radio_resolved = new System.Windows.Forms.RadioButton();
+            this.Radio_Pending = new System.Windows.Forms.RadioButton();
+            this.Radio_Forwarded = new System.Windows.Forms.RadioButton();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
             this.admin_panel.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
             // admin_panel
@@ -62,27 +63,44 @@ namespace Complaint_System.GUI
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.Accept_Btn);
-            this.tabPage1.Controls.Add(this.Decline_Btn);
-            this.tabPage1.Controls.Add(this.txtcnic);
-            this.tabPage1.Controls.Add(this.txtPhn);
-            this.tabPage1.Controls.Add(this.txtPassword);
-            this.tabPage1.Controls.Add(this.txt_name);
-            this.tabPage1.Controls.Add(this.label4);
-            this.tabPage1.Controls.Add(this.label3);
-            this.tabPage1.Controls.Add(this.label2);
-            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.Delete_Btn);
             this.tabPage1.Controls.Add(this.dataGridView1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(646, 318);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Cust Verify";
+            this.tabPage1.Text = "Customers";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Delete_Btn
+            // 
+            this.Delete_Btn.Location = new System.Drawing.Point(286, 255);
+            this.Delete_Btn.Name = "Delete_Btn";
+            this.Delete_Btn.Size = new System.Drawing.Size(81, 39);
+            this.Delete_Btn.TabIndex = 9;
+            this.Delete_Btn.Text = "Delete";
+            this.Delete_Btn.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(604, 173);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.btn_update);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Controls.Add(this.Radio_Forwarded);
+            this.tabPage2.Controls.Add(this.Radio_Pending);
+            this.tabPage2.Controls.Add(this.radio_resolved);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -93,6 +111,7 @@ namespace Complaint_System.GUI
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.dataGridView3);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
@@ -101,112 +120,80 @@ namespace Complaint_System.GUI
             this.tabPage3.Text = "View Complaints";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // radio_resolved
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(460, 27);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(166, 271);
-            this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.radio_resolved.AutoSize = true;
+            this.radio_resolved.Location = new System.Drawing.Point(240, 161);
+            this.radio_resolved.Name = "radio_resolved";
+            this.radio_resolved.Size = new System.Drawing.Size(70, 17);
+            this.radio_resolved.TabIndex = 0;
+            this.radio_resolved.TabStop = true;
+            this.radio_resolved.Text = "Resolved";
+            this.radio_resolved.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // Radio_Pending
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 47);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Name:";
+            this.Radio_Pending.AutoSize = true;
+            this.Radio_Pending.Location = new System.Drawing.Point(240, 206);
+            this.Radio_Pending.Name = "Radio_Pending";
+            this.Radio_Pending.Size = new System.Drawing.Size(64, 17);
+            this.Radio_Pending.TabIndex = 1;
+            this.Radio_Pending.TabStop = true;
+            this.Radio_Pending.Text = "Pending";
+            this.Radio_Pending.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // Radio_Forwarded
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 213);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "CNIC:";
+            this.Radio_Forwarded.AutoSize = true;
+            this.Radio_Forwarded.Location = new System.Drawing.Point(240, 250);
+            this.Radio_Forwarded.Name = "Radio_Forwarded";
+            this.Radio_Forwarded.Size = new System.Drawing.Size(75, 17);
+            this.Radio_Forwarded.TabIndex = 2;
+            this.Radio_Forwarded.TabStop = true;
+            this.Radio_Forwarded.Text = "Forwarded";
+            this.Radio_Forwarded.UseVisualStyleBackColor = true;
             // 
-            // label3
+            // dataGridView2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 156);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Phone no:";
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(379, 27);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(231, 269);
+            this.dataGridView2.TabIndex = 3;
             // 
-            // label4
+            // btn_update
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(24, 102);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(56, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Password:";
+            this.btn_update.Location = new System.Drawing.Point(109, 273);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(75, 23);
+            this.btn_update.TabIndex = 4;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = true;
             // 
-            // txt_name
+            // dataGridView3
             // 
-            this.txt_name.Location = new System.Drawing.Point(89, 44);
-            this.txt_name.Name = "txt_name";
-            this.txt_name.Size = new System.Drawing.Size(158, 20);
-            this.txt_name.TabIndex = 5;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(634, 306);
+            this.dataGridView3.TabIndex = 0;
             // 
-            // txtPassword
-            // 
-            this.txtPassword.Location = new System.Drawing.Point(89, 99);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(158, 20);
-            this.txtPassword.TabIndex = 6;
-            // 
-            // txtPhn
-            // 
-            this.txtPhn.Location = new System.Drawing.Point(89, 153);
-            this.txtPhn.Name = "txtPhn";
-            this.txtPhn.Size = new System.Drawing.Size(158, 20);
-            this.txtPhn.TabIndex = 7;
-            // 
-            // txtcnic
-            // 
-            this.txtcnic.Location = new System.Drawing.Point(89, 210);
-            this.txtcnic.Name = "txtcnic";
-            this.txtcnic.Size = new System.Drawing.Size(158, 20);
-            this.txtcnic.TabIndex = 8;
-            // 
-            // Decline_Btn
-            // 
-            this.Decline_Btn.Location = new System.Drawing.Point(89, 275);
-            this.Decline_Btn.Name = "Decline_Btn";
-            this.Decline_Btn.Size = new System.Drawing.Size(75, 23);
-            this.Decline_Btn.TabIndex = 9;
-            this.Decline_Btn.Text = "Decline";
-            this.Decline_Btn.UseVisualStyleBackColor = true;
-            // 
-            // Accept_Btn
-            // 
-            this.Accept_Btn.Location = new System.Drawing.Point(172, 275);
-            this.Accept_Btn.Name = "Accept_Btn";
-            this.Accept_Btn.Size = new System.Drawing.Size(75, 23);
-            this.Accept_Btn.TabIndex = 10;
-            this.Accept_Btn.Text = "Accept";
-            this.Accept_Btn.UseVisualStyleBackColor = true;
-            // 
-            // admin_Form
+            // adminPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.admin_panel);
-            this.Name = "admin_Form";
+            this.Name = "adminPanel";
             this.Text = "admin_Form";
             this.admin_panel.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -218,15 +205,12 @@ namespace Complaint_System.GUI
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.Button Accept_Btn;
-        private System.Windows.Forms.Button Decline_Btn;
-        private System.Windows.Forms.TextBox txtcnic;
-        private System.Windows.Forms.TextBox txtPhn;
-        private System.Windows.Forms.TextBox txtPassword;
-        private System.Windows.Forms.TextBox txt_name;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Delete_Btn;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.RadioButton Radio_Forwarded;
+        private System.Windows.Forms.RadioButton Radio_Pending;
+        private System.Windows.Forms.RadioButton radio_resolved;
+        private System.Windows.Forms.DataGridView dataGridView3;
     }
 }
